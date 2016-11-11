e0c4703dfbf342d5e8f77578d5567ef2257ccb3f
Leonardo Gomes <leogomes26@hotmail.com>
2016-11-11 01:00:44 -0200

    Correção no nome do packege das classes.

--- src/main/java/br/com/fpu/entity/Jogador.java
+++ src/main/java/br/com/fpu/entity/Jogador.java
@@ -1,6 +1,9 @@
-package com.example.domain;
+package br.com.fpu.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 import java.sql.Time;
 import javax.persistence.Id;
 
@@ -8,24 +11,58 @@ import javax.persistence.Id;
 public class Jogador {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer id_jogador;
-	private String nome_jogador;
-	private Time hora_inicial;
-	private Time hora_final;
+	private Integer idJogador;
+	private String nomeJogador;
+	private Time horaInicial;
+	private Time horaFinal;
 	private Integer placar;
-
-public Jogador(){
 	
-}
-
-public Jogador(Integer id_jogardor, String nome_jogador, Time hora_inicial, Time hora_final, Integer placar){
 	
-	this.id_jogador = id_jogador;
-	this.nome_jogador = nome_jogador;
-	this.hora_inicial = hora_inicial;
-	this.hora_final = hora_final;
-	this.placar = placar;
-}
+	
+	public Jogador() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Jogador(Integer idJogador, String nomeJogador, Time horaInicial, Time horaFinal, Integer placar) {
+		super();
+		this.idJogador = idJogador;
+		this.nomeJogador = nomeJogador;
+		this.horaInicial = horaInicial;
+		this.horaFinal = horaFinal;
+		this.placar = placar;
+	}
+	public Integer getIdJogador() {
+		return idJogador;
+	}
+	public void setIdJogador(Integer idJogador) {
+		this.idJogador = idJogador;
+	}
+	public String getNomeJogador() {
+		return nomeJogador;
+	}
+	public void setNomeJogador(String nomeJogador) {
+		this.nomeJogador = nomeJogador;
+	}
+	public Time getHoraInicial() {
+		return horaInicial;
+	}
+	public void setHoraInicial(Time horaInicial) {
+		this.horaInicial = horaInicial;
+	}
+	public Time getHoraFinal() {
+		return horaFinal;
+	}
+	public void setHoraFinal(Time horaFinal) {
+		this.horaFinal = horaFinal;
+	}
+	public Integer getPlacar() {
+		return placar;
+	}
+	public void setPlacar(Integer placar) {
+		this.placar = placar;
+	}
+
+
 	
 	
 }
--- src/main/java/br/com/fpu/entity/Pergunta.java
+++ src/main/java/br/com/fpu/entity/Pergunta.java
@@ -1,37 +1,76 @@
-package com.example.domain;
+package br.com.fpu.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.hibernate.annotations.GeneratorType;
 
 @Entity
 public class Pergunta {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer id_pergunta;
-	private String descricao_pergunta;
-	private String resposta_a;
-	private String resposta_b;
-	private String resposta_c;
-	private String resposta_correta;
+	private Integer idPergunta;
+	private String descricaoPergunta;
+	private String respostaA;
+	private String respostaB;
+	private String respostaC;
+	private String respostaCorreta;	
 	
-public Pergunta(){
 	
-}	
-
-public Pergunta(Integer id_pergunta, String descricao_pergunta, String resposta_a, String resposta_b, String resposta_c, String resposta_correta){
+	public Pergunta() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Pergunta(Integer idPergunta, String descricaoPergunta, String respostaA, String respostaB, String respostaC,
+			String respostaCorreta) {
+		super();
+		this.idPergunta = idPergunta;
+		this.descricaoPergunta = descricaoPergunta;
+		this.respostaA = respostaA;
+		this.respostaB = respostaB;
+		this.respostaC = respostaC;
+		this.respostaCorreta = respostaCorreta;
+	}
+	public Integer getIdPergunta() {
+		return idPergunta;
+	}
+	public void setIdPergunta(Integer idPergunta) {
+		this.idPergunta = idPergunta;
+	}
+	public String getDescricaoPergunta() {
+		return descricaoPergunta;
+	}
+	public void setDescricaoPergunta(String descricaoPergunta) {
+		this.descricaoPergunta = descricaoPergunta;
+	}
+	public String getRespostaA() {
+		return respostaA;
+	}
+	public void setRespostaA(String respostaA) {
+		this.respostaA = respostaA;
+	}
+	public String getRespostaB() {
+		return respostaB;
+	}
+	public void setRespostaB(String respostaB) {
+		this.respostaB = respostaB;
+	}
+	public String getRespostaC() {
+		return respostaC;
+	}
+	public void setRespostaC(String respostaC) {
+		this.respostaC = respostaC;
+	}
+	public String getRespostaCorreta() {
+		return respostaCorreta;
+	}
+	public void setRespostaCorreta(String respostaCorreta) {
+		this.respostaCorreta = respostaCorreta;
+	}
 	
-	this.id_pergunta = id_pergunta;
-	this.descricao_pergunta = descricao_pergunta;
-	this.resposta_a = resposta_a;
-	this.resposta_b = resposta_b;
-	this.resposta_c = resposta_c;
-	this.resposta_correta = resposta_correta;
 	
-}
+	
 	
 }
